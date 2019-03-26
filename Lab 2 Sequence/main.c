0868fd41f0212b775a157fb4c2ea3652876f01a1
IlyaDerevyanchenko <47447740+IlyaDerevyanchenko@users.noreply.github.com>
2019-03-26 05:40:31 +0200

    Add files via upload

---
+++ Lab 2 Sequence/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int N;
+    double x, leftX = -1, rightX = 1;
+
+    printf("Enter x, N: ");
+    if (scanf("%lf %d", &x, &N) != 2) {
+        printf("Invalid input\n");
+        return -1;
+    }
+
+    if (x < leftX || x > rightX) {
+        char* fmt = "Incorrect %.4f. Need from [%.4f, %.4f]\n";
+        printf(fmt, x, leftX, rightX);
+        return -1;
+    }
+
+    double a = x, S = a;
+
+    for (int n = 1; n <= N; n++) {
+        double q = ((2*x*x*n)-(x*x))/(2*n+1);
+        a *= q;
+        S += a;
+    }
+    //Calc result
+
+    double y = -log((1-x)/(1+x))/2;
+    printf("S=%.7f\n", S);
+    printf("y=%.7f\n", y);
+    double eps = fabs(y-S)/fabs(S)*100;
+    printf("e=%.2f%%\n", eps);
+
+    return 0;
+}
