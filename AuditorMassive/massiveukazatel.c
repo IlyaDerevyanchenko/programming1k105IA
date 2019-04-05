1a325ea98fd6ec117f311eacbb9ed4674a5d4b20
IlyaDerevyanchenko <47447740+IlyaDerevyanchenko@users.noreply.github.com>
2019-04-06 01:18:23 +0300

    Add files via upload

---
+++ AuditorMassive/massiveukazatel.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    double arr[20];
+    int neg = 0;
+    int i = 0;
+    while (scanf("%lf", &arr[i++])){};
+    arr [i-1] = NAN;
+    for (i=0; !isnan(arr[i]);++i){
+
+
+      if (arr[i]<0)
+        {
+         ++neg;
+             printf("Negative: %.2f\n", arr[i]);
+        }
+else{
+            printf("Positive: %.2f\n", arr[i]);}
+    }
+    printf("kol-wo otricatelnyh: %.d\n", neg);
+    return 0;
+}
